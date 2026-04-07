aa4b07f86a61878c4dec9c1d4b6a4efc3a1396bb
void0x14 <aulac785@gmail.com>
2026-04-07 03:37:20 +0300

    chore: remove accidentally committed binaries and unrelated docs

--- tests/test_enum.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-pub fn main() void {
-    const info = @typeInfo(std.zig.SanitizeC);
-    for (info.Enum.fields) |f| {
-        std.debug.print("{s}\n", .{f.name});
-    }
-}
--- tests/test_fork.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-pub fn main() !void {
-    const pid = try std.posix.fork();
-    if (pid == 0) {
-        std.posix.exit(0);
-    }
-}
